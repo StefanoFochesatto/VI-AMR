ea4ae1ee137f651201d0f5dc937b80ed57f7f0b5
Ed Bueler <elbueler@alaska.edu>
2024-11-30 22:28:40 -0800

    initial UDO implementation in class VIAMR

--- src/sphere.py
+++ src/sphere.py
@@ -12,16 +12,8 @@ from netgen.geom2d import SplineGeometry
 
 refinements = 3
 debugoutputs = False
-
-# Generate initial mesh using netgen
 width = 2
 TriHeight = 0.4
-geo = SplineGeometry()
-geo.AddRectangle(p1=(-1 * width, -1 * width), p2=(1 * width, 1 * width), bc="rectangle")
-ngmsh = geo.GenerateMesh(maxh=TriHeight)
-mesh = Mesh(ngmsh)
-mesh.topology_dm.viewFromOptions("-dm_view")
-
 
 def sphere_problem(mesh, V):
     # exactly-solvable obstacle problem for spherical obstacle
@@ -43,64 +35,76 @@ def sphere_problem(mesh, V):
     return psi, gbdry
 
 
-for i in range(refinements + 1):
-    # get initial iterate from previous mesh (or zero when i==0)
-    print(f"level {i}")
-    V = FunctionSpace(mesh, "CG", 1)
-    if i == 0:
-        u = Function(V)  # set to zero
-    else:
-        # cross-mesh interpolation
-        u = Function(V).interpolate(u)
-    u.rename("solution u")
+for method in ['udo', 'vces']:
+    # re-generate initial mesh using netgen
+    geo = SplineGeometry()
+    geo.AddRectangle(p1=(-1 * width, -1 * width), p2=(1 * width, 1 * width), bc="rectangle")
+    ngmsh = None
+    ngmsh = geo.GenerateMesh(maxh=TriHeight)
+    mesh = Mesh(ngmsh)  # there seems to be no mesh.deep_copy() or similar
+    for i in range(refinements + 1):
+        # get initial iterate from previous mesh (or zero when i==0)
+        print(f"{method} level {i}")
+        V = FunctionSpace(mesh, "CG", 1)
+        if i == 0:
+            u = Function(V)  # set to zero
+        else:
+            # cross-mesh interpolation
+            u = Function(V).interpolate(u)
+        u.rename("solution u")
+        print(f"  mesh has {mesh.num_cells()} elements")
 
-    # set up weak form problem; F is residual operator in nonlinear system F==0
-    lbound, gbdry = sphere_problem(mesh, V)
-    lbound.rename("obstacle psi")
-    v = TestFunction(V)
-    # interior condition of VI is Laplace equation  - div (grad u) = 0
-    F = inner(grad(u), grad(v)) * dx
-    bdry_ids = (1, 2, 3, 4)  # all four sides of boundary
-    bcs = DirichletBC(V, gbdry, bdry_ids)
+        # set up weak form problem; F is residual operator in nonlinear system F==0
+        lbound, gbdry = sphere_problem(mesh, V)
+        lbound.rename("obstacle psi")
+        v = TestFunction(V)
+        # interior condition of VI is Laplace equation  - div (grad u) = 0
+        F = inner(grad(u), grad(v)) * dx
+        bdry_ids = (1, 2, 3, 4)  # all four sides of boundary
+        bcs = DirichletBC(V, gbdry, bdry_ids)
 
-    # set up nonlinear solver
-    # Note that VI problems are inherently nonlinear.  This solver is from PETSc's
-    # SNES component, a VI-adapted line search Newton method called "vinewtonrsls"
-    # (Benson & Munson, 2006).
-    sp = {
-        "snes_type": "vinewtonrsls",
-        "snes_converged_reason": None,
-        # "snes_monitor": None,
-        # "snes_vi_monitor": None, # prints bounds info for each Newton iteration
-        "snes_rtol": 1.0e-8,
-        "snes_atol": 1.0e-12,
-        "snes_stol": 0.0,
-        "snes_vi_zero_tolerance": 1.0e-12,
-        "snes_linesearch_type": "basic",
-        "ksp_type": "preonly",
-        "pc_type": "lu",
-        "pc_factor_mat_solver_type": "mumps",
-    }
-    problem = NonlinearVariationalProblem(F, u, bcs)
+        # set up nonlinear solver
+        # Note that VI problems are inherently nonlinear.  This solver is from PETSc's
+        # SNES component, a VI-adapted line search Newton method called "vinewtonrsls"
+        # (Benson & Munson, 2006).
+        sp = {
+            "snes_type": "vinewtonrsls",
+            "snes_converged_reason": None,
+            # "snes_monitor": None,
+            # "snes_vi_monitor": None, # prints bounds info for each Newton iteration
+            "snes_rtol": 1.0e-8,
+            "snes_atol": 1.0e-12,
+            "snes_stol": 0.0,
+            "snes_vi_zero_tolerance": 1.0e-12,
+            "snes_linesearch_type": "basic",
+            "ksp_type": "preonly",
+            "pc_type": "lu",
+            "pc_factor_mat_solver_type": "mumps",
+        }
+        problem = NonlinearVariationalProblem(F, u, bcs)
 
-    # solve VI problem
-    solver = NonlinearVariationalSolver(
-        problem, solver_parameters=sp, options_prefix=""
-    )
-    ubound = Function(V).interpolate(Constant(PETSc.INFINITY))  # no upper obstacle
-    solver.solve(bounds=(lbound, ubound))
-    if i == refinements:
-        break
+        # solve VI problem
+        solver = NonlinearVariationalSolver(
+            problem, solver_parameters=sp, options_prefix=""
+        )
+        ubound = Function(V).interpolate(Constant(PETSc.INFINITY))  # no upper obstacle
+        solver.solve(bounds=(lbound, ubound))
+        print(f"  L2 error norm = {errornorm(u, gbdry):.3e}")
+        if i == refinements:
+            break
 
-    # refine via VCES marking using default parameters
-    mark = VIAMR().vcesmark(mesh, u, lbound)
-    if debugoutputs:
-        VTKFile(f"result{i}.pvd").write(u, lbound, mark)
+        # refine via marking using default parameters
+        if method == 'udo':
+            mark = VIAMR().udomark(mesh, u, lbound)
+        else:
+            mark = VIAMR().vcesmark(mesh, u, lbound)
+        if debugoutputs:
+            VTKFile(f"result_{method}{i}.pvd").write(u, lbound, mark)
 
-    # use NetGen to get next mesh
-    mesh = mesh.refine_marked_elements(mark)
+        # use NetGen to get next mesh
+        mesh = mesh.refine_marked_elements(mark)
 
-# write results
-uexact = gbdry.copy()
-uerror = Function(V, name="u error").interpolate(abs(u - uexact))
-VTKFile(f"result.pvd").write(u, lbound, uerror)
+    # write results
+    uexact = gbdry.copy()
+    uerror = Function(V, name="u error").interpolate(abs(u - uexact))
+    VTKFile(f"result_{method}.pvd").write(u, lbound, uerror)
--- src/viamr.py
+++ src/viamr.py
@@ -1,23 +1,86 @@
 import numpy as np
+from collections import deque
 from firedrake import *
 from firedrake.petsc import OptionsManager
 from firedrake.output import VTKFile
 
 
 class VIAMR(OptionsManager):
+
     def __init__(self, **kwargs):
         # solver_parameters = flatten_parameters(kwargs.pop("solver_parameters",{}))
         # self.options_prefix = kwargs.pop("options_prefix","")
         # super().__init__(solver_parameters, self.options_prefix)  # set PETSc parameters
-        pass
+        self.activetol = 1.0e-10
 
-    def vcesmark(self, cmesh, u, lb, tolerance=1e-10, bracket=[0.2, 0.8]):
+    def spaces(self, mesh, p=1):
+        return FunctionSpace(mesh, "CG", p), FunctionSpace(mesh, "DG", p-1)
+
+    def nodalactive(self, u, lb):
+        z = Function(u.function_space(), name="Nodal Active Set Indicator")
+        z.interpolate(conditional(abs(u - lb) < self.activetol, 0, 1))
+        return z
+
+    def bfs_neighbors(self, cmesh, border, levels, active):
+        '''element-wise Fast Multi Neighbor Lookup BFS can Avoid Active Set'''
+
+        # dictionary to map each vertex to the cells that contain it
+        vertex_to_cells = {}
+        cell_vertex_map = cmesh.topology.cell_closure # cell to vertex connectivity
+        # Loop over all cells to populate the dictionary
+        for i in range(cmesh.num_cells()):
+            # first three entries correspond to the vertices
+            for vertex in cell_vertex_map[i][:3]:
+                if vertex not in vertex_to_cells:
+                    vertex_to_cells[vertex] = []
+                vertex_to_cells[vertex].append(i)
+
+        # Loop over all cells to mark neighbors
+        # Create a new DG0 function to store the result
+        result = Function(border.function_space(), name='nNeighbors')
+        for i in range(cmesh.num_cells()):
+            # If the function value is 1 and the cell is in the active set
+            if border.dat.data[i] == 1 and active.dat.data[i] == 0:
+                # Use a BFS algorithm to find all cells within the specified number of levels
+                queue = deque([(i, 0)])
+                visited = set()
+                while queue:
+                    cell, level = queue.popleft()
+                    if cell not in visited and level <= levels:
+                        visited.add(cell)
+                        result.dat.data[cell] = 1
+                        for vertex in cell_vertex_map[cell][:3]:
+                            for neighbor in vertex_to_cells[vertex]:
+                                queue.append((neighbor, level + 1))
+        return result
+
+    def udomark(self, cmesh, u, lb, n=2):
+        "Mark current mesh using Unstructured Dilation Operator (UDO) algorithm."
+
+        # generate element-wise and nodal-wise indicators for active set
+        CG1, DG0 = self.spaces(cmesh)
+        nodaldiffCG = Function(CG1).interpolate(abs(u - lb))
+        elemactive = Function(DG0, name="Element Active Set Indicator")
+        elemactive.interpolate(conditional(nodaldiffCG < self.activetol, 1, 0))
+        nodalactive = self.nodalactive(u, lb)
+
+        # Define Border Elements Set
+        elemborder = Function(DG0, name="Border Elements")
+        elemborder.interpolate(conditional(nodalactive > 0,
+                                           conditional(nodalactive < 1, 1, 0),
+                                           0))
+
+        # bfs_neighbors() constructs N^n(B) indicator.  Last argument
+        # is to refine only in active or only in inactive set (currently commented out).
+        return self.bfs_neighbors(cmesh, elemborder, n, elemactive)
+
+
+    def vcesmark(self, cmesh, u, lb, bracket=[0.2, 0.8]):
         "Mark current mesh using Variable Coefficient Elliptic Smoothing (VCES) algorithm"
 
         # Compute nodal active set indicator within some tolerance
-        CG1 = FunctionSpace(cmesh, "CG", 1)
-        nodalactive = Function(CG1, name="Nodal Active Set Indicator")
-        nodalactive.interpolate(conditional(abs(u - lb) < tolerance, 0, 1))
+        CG1, DG0 = self.spaces(cmesh)
+        nodalactive = self.nodalactive(u, lb)
 
         # Vary timestep by average cell area of each patch.
         # Not exactly an average because msh.cell_sizes is an L2 projection of
@@ -46,7 +109,3 @@ class VIAMR(OptionsManager):
             conditional(uDG0 > bracket[0], conditional(uDG0 < bracket[1], 1, 0), 0)
         )
         return mark
-
-    def udomark(self, cmesh):
-        "Mark current mesh using Unstructured Dilation Operator (UDO) algorithm"
-        raise NotImplementedError  # TODO
