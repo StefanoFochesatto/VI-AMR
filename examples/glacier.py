4e7774e9e3821d2ba689b004bd0d9995cbd2f61e
Ed Bueler <elbueler@alaska.edu>
2024-12-10 14:10:21 -0900

    VIAMR for an ice sheet.
    Uses VCES because it works in parallel.  Current schedule
    is alternation of VCES AMR and uniform refinement.
    See TODO at top of file for little things I will work on.

--- examples/glacier.py
+++ examples/glacier.py
@@ -1,35 +1,55 @@
+# TODO * instrument with number of elements reported at each level
+#      * also report range of h values?
+#      * new option: -cases {cap,mountain,dome};  cap and dome are current;
+#            mountain splits into pieces (by modifying a) and has bigger bumps
+#      * allow other strategies than alternating AMR & uniform?
+
+# Hi-resolution parallel run:
+#   mpiexec -n 16 python3 glacier.py -refine 8 -snes_rtol 1.0e-10 -snes_monitor -opvd hires.pvd
+# One level uniform comparison?? (need to count elements etc to compare):
+#   mpiexec -n 16 python3 glacier.py -onelevel -m 800 -snes_rtol 1.0e-10 -snes_monitor -opvd hires_onelevel.pvd
+
 from argparse import ArgumentParser, RawTextHelpFormatter
 parser = ArgumentParser(description="""
 Solves 2D steady shallow ice approximation obstacle problem on a square [0,L]^2
-where L = 1800.0 km.  Default behavior is to solve for the "dome" exact
-solution, on a flat bed.  Optionally (-bumps) generates a random, but smooth,
-bed topography; there is no exact solution in this case.  Note there is a double
-regularization in the isothermal, Glen-law diffusivity; see options -epsH and -epsplap.
-Default solver is vinewtonrsls + mumps, iterated to convergence.
-Example runs with achievable resolution:
-  python3 glacier.py -m 80 -opvd dome.pvd
-  python3 glacier.py -m 80 -bumps -opvd bumps.pvd
+where L = 1800.0 km.  By default generates a random, but smooth, bed topography.
+Option -dome solves in a flat bed case where the exact solution is known.  Note
+there is a double regularization in the isothermal, Glen-law diffusivity; see
+options -epsH and -epsplap.  Solver is vinewtonrsls + mumps.  Applies VIAMR
+VCES method (because UD0 is not currently parallel) alternately with uniform
+refinement.  Examples:
+  python3 glacier.py -dome -opvd dome.pvd
+  python3 glacier.py -opvd bumps.pvd
 """, formatter_class=RawTextHelpFormatter)
-parser.add_argument('-bumps', action='store_true', default=False,
-                    help='generate bumpy bed topography')
+parser.add_argument('-dome', action='store_true', default=False,
+                    help='generate dome case, which has exact solution')
 parser.add_argument('-epsH', type=float, default=20.0, metavar='X',
                     help='diffusivity regularization for thickness [default 20.0 m]')
 parser.add_argument('-epsplap', type=float, default=1.0e-4, metavar='X',
                     help='diffusivity regularization for p-Laplacian [default 1.0e-4]')
-parser.add_argument('-m', type=int, default=20, metavar='MC',
+parser.add_argument('-m', type=int, default=20, metavar='M',
                     help='number of cells in each direction [default=20]')
 parser.add_argument('-opvd', metavar='FILE', type=str, default='',
                     help='output file name for Paraview format (.pvd)')
 parser.add_argument('-qdegree', type=int, default=4, metavar='DEG',
                     help='quadrature degree used in SIA nonlinear weak form [default 4]')
+parser.add_argument('-refine', type=int, default=3, metavar='R',
+                    help='number of AMR refinements')
+parser.add_argument('-onelevel', action='store_true', default=False,
+                    help='no AMR refinements; use Firedrake to generate uniform mesh')
 args, passthroughoptions = parser.parse_known_args()
 
-assert args.m >= 1, 'at least one cell in mesh'
-
+import numpy as np
 import petsc4py
 petsc4py.init(passthroughoptions)
 from firedrake import *
+from firedrake.petsc import PETSc
 from firedrake.output import VTKFile
+
+assert args.m >= 1, 'at least one cell in mesh'
+if args.onelevel:
+    args.refine = 0
+    PETSc.Sys.Print('not using refinement; uniform mesh generated with Firedrake')
 
 L = 1800.0e3        # domain is [0,L]^2, with fields centered at (xc,xc)
 xc = L/2
@@ -80,71 +100,114 @@ def bumps(x):
         + 0.5 * sin(19*pi*xx) * sin(11*pi*yy) + 0.5 * sin(12*pi*xx) * sin(17*pi*yy)
     return B0 * b
 
-mesh = RectangleMesh(args.m, args.m, L, L)
-x = SpatialCoordinate(mesh)
-
-V = FunctionSpace(mesh, "CG", 1)
-s = Function(V, name="s = surface elevation")  # initialize to zero
-a = Function(V).interpolate(accumulation(x))
-a.rename("a = accumulation")
-
-if args.bumps:
-    lb = Function(V).interpolate(bumps(x))
-    lb.rename("lb = bedrock topography")
+# generate first mesh
+PETSc.Sys.Print(f'generating {args.m} x {args.m} initial mesh ...')
+if args.onelevel:
+    # generate via Firedrake
+    mesh = RectangleMesh(args.m, args.m, L, L)
 else:
-    lb = Function(V).interpolate(Constant(0))
-    sexact = Function(V).interpolate(dome_exact(x))
-    sexact.rename("s_exact")
+    # prepare for AMR by generating via netgen
+    from viamr import VIAMR
+    try:
+        import netgen
+    except ImportError:
+        print("ImportError.  Unable to import NetGen.  Exiting.")
+        import sys
+        sys.exit(0)
+    from netgen.geom2d import SplineGeometry
+    geo = SplineGeometry()
+    geo.AddRectangle(p1=(0.0, 0.0), p2=(L, L), bc="rectangle")
+    ngmsh = None
+    trih = L / args.m
+    ngmsh = geo.GenerateMesh(maxh=trih)
+    mesh = Mesh(ngmsh)
 
-v = TestFunction(V)
-Dplap = (args.epsplap**2 + dot(grad(s), grad(s)))**((p-2)/2)
-F = Gamma * (s - lb + args.epsH)**(p+1) * Dplap * inner(grad(s), grad(v)) * dx(degree=args.qdegree) \
-    - inner(a, v) * dx
-bcs = DirichletBC(V, 0, "on_boundary")
-problem = NonlinearVariationalProblem(F, s, bcs)
-
-# initialize to pile of ice, from accumulation
-pilefactor = 400.0
-s.interpolate(lb + pilefactor * secpera * conditional(a > 0.0, a, 0.0))
-
+# solver parameters
 sp = {"snes_type": "vinewtonrsls",
     "snes_rtol": 1.0e-4,  # low regularity implies lowered expectations
     "snes_atol": 1.0e-12,
     "snes_stol": 1.0e-5,
-    "snes_monitor": None,
+    #"snes_monitor": None,
+    "snes_converged_reason": None,
     "snes_linesearch_type": "bt",
     "snes_linesearch_order": "1",
     "snes_max_it": 1000,
     "ksp_type": "preonly",
     "pc_type": "lu",
     "pc_factor_mat_solver_type": "mumps"}
-solver = NonlinearVariationalSolver(problem, solver_parameters=sp, options_prefix="")
-solver.solve(bounds=(lb, Function(V).interpolate(Constant(PETSc.INFINITY))))
 
-from firedrake.petsc import PETSc
-PETSc.Sys.Print('done (%s): %d x %d mesh, h=%.3f km' % \
-                ('bumps' if args.bumps else 'dome', args.m, args.m, L / (1000 * args.m)))
+# main loop
+for i in range(args.refine + 1):
+    if i > 0:
+        # alternate: AMR,uniform,AMR,uniform,...
+        if np.mod(i, 2) == 1:
+            mark = VIAMR().vcesmark(mesh, s, lb)
+            #mark = VIAMR().udomark(mesh, s, lb, n=1)  # not in parallel, but otherwise great
+        else:
+            # mark everybody
+            W = FunctionSpace(mesh, "DG", 0)
+            mark = Function(W).interpolate(Constant(1.0))
+        # use NetGen to get next mesh
+        mesh = mesh.refine_marked_elements(mark)
 
-if not args.bumps:
-    sdiff = Function(V).interpolate(s - sexact)
-    sdiff.rename("sdiff = s - s_exact")
-    with sdiff.dat.vec_ro as v:
-        err_max = abs(v).max()[1]
-    err_av = assemble(abs(sdiff) * dx) / L**2
-    PETSc.Sys.Print('             |s-s_exact|_inf = %.3f m,  |s-s_exact|_av = %.3f m' \
-                    % (err_max, err_av))
+    # obstacle
+    V = FunctionSpace(mesh, "CG", 1)
+    x = SpatialCoordinate(mesh)
+    if args.dome:
+        lb = Function(V).interpolate(Constant(0))
+        sexact = Function(V).interpolate(dome_exact(x))
+        sexact.rename("s_exact")
+    else:
+        lb = Function(V).interpolate(bumps(x))
+        lb.rename("lb = bedrock topography")
+
+    # source term
+    a = Function(V).interpolate(accumulation(x))
+    a.rename("a = accumulation")
+
+    if i == 0:
+        # initialize as pile of ice, from accumulation
+        pilefactor = 400.0
+        sinit = lb + pilefactor * secpera * conditional(a > 0.0, a, 0.0)
+        s = Function(V).interpolate(sinit)
+    else:
+        # cross-mesh interpolation of previous solution
+        s = Function(V).interpolate(s)
+    s.rename("s = surface elevation")
+
+    # weak form
+    v = TestFunction(V)
+    Hreg = s - lb + args.epsH
+    Dplap = (args.epsplap**2 + dot(grad(s), grad(s)))**((p-2)/2)
+    F = Gamma * Hreg**(p+1) * Dplap * inner(grad(s), grad(v)) * dx(degree=args.qdegree) \
+        - inner(a, v) * dx
+    bcs = DirichletBC(V, 0, "on_boundary")
+    problem = NonlinearVariationalProblem(F, s, bcs)
+
+    # solve
+    solver = NonlinearVariationalSolver(problem, solver_parameters=sp, options_prefix="")
+    solver.solve(bounds=(lb, Function(V).interpolate(Constant(PETSc.INFINITY))))
+
+    PETSc.Sys.Print(f'done: level {i}')
+    if args.dome:
+        sdiff = Function(V).interpolate(s - dome_exact(x))
+        sdiff.rename("sdiff = s - s_exact")
+        err_l2 = norm(sdiff / L)
+        err_av = norm(sdiff, 'l1') / L**2
+        PETSc.Sys.Print('    |s-s_exact|_2 = %.3f m,  |s-s_exact|_av = %.3f m' \
+                        % (err_l2, err_av))
 
 if args.opvd:
     CU = ((n+2)/(n+1)) * Gamma
     U_ufl = CU * (s - lb)**p * inner(grad(s), grad(s))**((p-2)/2) * grad(s)
     U = Function(VectorFunctionSpace(mesh, 'CG', degree=2))
-    U.project(secpera * U_ufl)
+    U.project(secpera * U_ufl)  # smoother than .interpolate()
     U.rename("U = surface velocity (m/a)")
-    Q = Function(VectorFunctionSpace(mesh, 'CG', degree=1))
+    Q = Function(VectorFunctionSpace(mesh, 'DG', degree=1))
     Q.interpolate(U * (s - lb))
     Q.rename("Q = UH = volume flux (m^2/a)")
     PETSc.Sys.Print('writing to %s ...' % args.opvd)
-    if args.bumps:
-        VTKFile(args.opvd).write(a,s,U,Q,lb)
-    else:
+    if args.dome:
         VTKFile(args.opvd).write(a,s,U,Q,sexact,sdiff)
+    else:
+        VTKFile(args.opvd).write(a,s,U,Q,lb)
