ba836a1ff2e8dd3fff68374770a3bf7f1c2eecdb
StefanoFochesatto <54640332+StefanoFochesatto@users.noreply.github.com>
2024-12-13 01:00:18 -0600

    working metric based proof of concept

--- examples/metricintersection.py
+++ examples/metricintersection.py
@@ -1,10 +1,24 @@
-'''Run UDO and VCES methods on two obstacle problems.
-"sphere" problem has exact solution, "spiral" does not.
-Generates 4 .pvd output files.'''
-
+from netgen.geom2d import SplineGeometry
 import numpy as np
 from firedrake import *
+# I found the best way to install animate is to clone to the src folder
+# and make install with the firedrake env activated
+# git clone https://github.com/mesh-adaptation/animate.git
+# cd animate
+# make install
+
+from animate import *
+
 from viamr import VIAMR
+# Subclass VIAMR to return u
+
+
+class VIAMRMetric(VIAMR):
+    def vcesmark(self, *args, **kwargs):
+        # Call the original method
+        mark = super().vcesmark(*args, **kwargs)
+        return mark, u
+
 
 try:
     import netgen
@@ -12,14 +26,30 @@ except ImportError:
     print("ImportError.  Unable to import NetGen.  Exiting.")
     import sys
     sys.exit(0)
-from netgen.geom2d import SplineGeometry
 
-refinements = 3
+
 debugoutputs = False
 
-problems = ['sphere', 'spiral']
-widths = [2.0, 1.0]
-TriHeights = [0.4, 0.1]
+width = 2.0
+TriHeight = 0.05
+
+metric_params = {
+    "dm_plex_metric": {
+        "target_complexity": 3000.0,
+        "p": 2.0,  # normalisation order
+        "h_min": 1e-06,  # minimum allowed edge length
+        "h_max": 1.0,  # maximum allowed edge length
+    }
+}
+
+
+def metricfromhessian(mesh, u):
+    P1_ten = TensorFunctionSpace(mesh, "CG", 1)
+    metric = RiemannianMetric(P1_ten)
+    metric.set_parameters(metric_params)
+    metric.compute_hessian(u)
+    metric.normalise()
+    return metric
 
 
 def get_mesh(width, TriHeight):
@@ -54,94 +84,56 @@ def sphere_problem(mesh, V):
     return psi, gbdry, uexact
 
 
-def spiral_problem(mesh, V):
-    # spiral obstacle problem from section 7.1.1 in Graeser & Kornhuber (2009)
-    (x, y) = SpatialCoordinate(mesh)
-    r = sqrt(x * x + y * y)
-    theta = atan2(y, x)
-    tmp = sin(2.0*pi/r + pi/2.0 - theta) + r * \
-        (r+1) / (r - 2.0) - 3.0 * r + 3.6
-    psi = Function(V).interpolate(conditional(le(r, 1.0e-8), 3.6, tmp))
-    gbdry = Constant(0.0)
-    uexact = None  # not known
-    return psi, gbdry, uexact
+mesh = get_mesh(width, TriHeight)
+for i in range(3):
+    V = FunctionSpace(mesh, "CG", 1)
+    if i == 0:
+        u = Function(V)  # set to zero
+    else:
+        # cross-mesh interpolation
+        u = Function(V).interpolate(u)
+    u.rename("solution u")
 
+    # Weak form and boundary conditions and obstacle
+    lbound, gbdry, uexact = sphere_problem(mesh, V)
+    lbound.rename("obstacle psi")
+    v = TestFunction(V)
+    F = inner(grad(u), grad(v)) * dx
+    bdry_ids = (1, 2, 3, 4)  # all four sides of boundary
+    bcs = DirichletBC(V, gbdry, bdry_ids)
 
-for prob in range(2):
-    mesh = get_mesh(widths[prob], TriHeights[prob])
-    for i in range(refinements + 1):
-        # get initial iterate from previous mesh (or zero when i==0)
-        print(f"{problems[prob]} {'vces'} level {i}")
-        V = FunctionSpace(mesh, "CG", 1)
-        if i == 0:
-            u = Function(V)  # set to zero
-        else:
-            # cross-mesh interpolation
-            u = Function(V).interpolate(u)
-        u.rename("solution u")
-        VIAMR().meshreport(mesh)
+    # set up nonlinear solver
+    sp = {
+        "snes_type": "vinewtonrsls",
+        "snes_converged_reason": None,
+        # "snes_monitor": None,
+        # "snes_vi_monitor": None, # prints bounds info for each Newton iteration
+        "snes_rtol": 1.0e-8,
+        "snes_atol": 1.0e-12,
+        "snes_stol": 0.0,
+        "snes_vi_zero_tolerance": 1.0e-12,
+        "snes_linesearch_type": "basic",
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps",
+    }
+    problem = NonlinearVariationalProblem(F, u, bcs)
+    solver = NonlinearVariationalSolver(
+        problem, solver_parameters=sp, options_prefix=""
+    )
+    ubound = Function(V).interpolate(
+        Constant(PETSc.INFINITY))  # no upper obstacle
+    solver.solve(bounds=(lbound, ubound))
 
-        # set up weak form problem; F is residual operator in nonlinear system F==0
-        if prob == 0:
-            lbound, gbdry, uexact = sphere_problem(mesh, V)
-        else:
-            lbound, gbdry, uexact = spiral_problem(mesh, V)
-        lbound.rename("obstacle psi")
-        v = TestFunction(V)
-        # interior condition of VI is Laplace equation  - div (grad u) = 0
-        F = inner(grad(u), grad(v)) * dx
-        bdry_ids = (1, 2, 3, 4)  # all four sides of boundary
-        bcs = DirichletBC(V, gbdry, bdry_ids)
+    _, freeboundaryfield = VIAMRMetric().vcesmark(
+        mesh, u, lbound, bracket=[0.06, 0.94])
 
-        # set up nonlinear solver
-        # Note that VI problems are inherently nonlinear.  This solver is from PETSc's
-        # SNES component, a VI-adapted line search Newton method called "vinewtonrsls"
-        # (Benson & Munson, 2006).
-        sp = {
-            "snes_type": "vinewtonrsls",
-            "snes_converged_reason": None,
-            # "snes_monitor": None,
-            # "snes_vi_monitor": None, # prints bounds info for each Newton iteration
-            "snes_rtol": 1.0e-8,
-            "snes_atol": 1.0e-12,
-            "snes_stol": 0.0,
-            "snes_vi_zero_tolerance": 1.0e-12,
-            "snes_linesearch_type": "basic",
-            "ksp_type": "preonly",
-            "pc_type": "lu",
-            "pc_factor_mat_solver_type": "mumps",
-        }
-        problem = NonlinearVariationalProblem(F, u, bcs)
+    solutionMetric = metricfromhessian(mesh, u)
+    freeboundaryMetric = metricfromhessian(mesh, freeboundaryfield)
 
-        # solve VI problem
-        solver = NonlinearVariationalSolver(
-            problem, solver_parameters=sp, options_prefix=""
-        )
-        ubound = Function(V).interpolate(
-            Constant(PETSc.INFINITY))  # no upper obstacle
-        solver.solve(bounds=(lbound, ubound))
-        if uexact is not None:
-            print(f"  L2 error norm = {errornorm(u, uexact):.3e}")
-        if i == refinements:
-            break
+    intersected_metric = solutionMetric.copy(deepcopy=True)
+    intersected_metric.average(freeboundaryMetric, weights=[.2, .8])
+    mesh_intersected = adapt(mesh, intersected_metric)
+    mesh = mesh_intersected
 
-        mark = VIAMR().vcesmark(mesh, u, lbound, bracket=[
-            0.06, 0.94])  # more refinement
-
-        if debugoutputs:
-            VTKFile(f"result_{problems[prob]}_{'vces'}{i}.pvd").write(
-                u, lbound, mark)
-
-        # use NetGen to get next mesh
-        mesh = mesh.refine_marked_elements(mark)
-
-        # write results
-        ugap = Function(V).interpolate(u - lbound)
-        ugap.rename('gap u - psi')
-        if uexact is not None:
-            uerror = Function(V, name="u error").interpolate(abs(u - uexact))
-            VTKFile(f"result_{problems[prob]}_{'vces'}.pvd").write(
-                u, lbound, ugap, uerror)
-        else:
-            VTKFile(f"result_{problems[prob]}_{'vces'}.pvd").write(
-                u, lbound, ugap)
+VTKFile("MetricMesh.pvd").write(mesh)
