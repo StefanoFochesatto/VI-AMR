5a260b5d7c997420b05d940faa56b6618ad1698a
StefanoFochesatto <54640332+StefanoFochesatto@users.noreply.github.com>
2024-12-26 12:29:45 -0900

    working isotropic gradient based example

--- examples/metricAdaptation.py
+++ examples/metricAdaptation.py
@@ -7,44 +7,12 @@ from TestProblem import ObstacleProblem
 from viamr import VIAMR
 
 
-class VIAMRMetric(VIAMR):
-    def vcesmark(self, *args, **kwargs):
-        # Call the original method
-        mark = super().vcesmark(*args, **kwargs)
-        return mark, u
-
-
 initTriHeight = .1
 
 problem_instance = ObstacleProblem(TriHeight=initTriHeight)
 u, lb, mesh = problem_instance.solveProblem(mesh=None, u=None)
-viamr = VIAMRMetric()
-_, s = viamr.vcesmark(mesh, u, lb)
+viamr = VIAMR()
+adaptedMesh = viamr.metricrefine(mesh, u, lb)
 
 
-V = FunctionSpace(mesh, "CG", 1)
-gs = Function(V).project(dot(grad(s), grad(s)))
-dim = mesh.topological_dimension()
-
-
-adapted = viamr.metricrefine(mesh, u, lb)
-
-P1_ten = TensorFunctionSpace(mesh, "CG", 1)
-metric = RiemannianMetric(P1_ten)
-
-mp = {
-    "dm_plex_metric": {
-        "target_complexity": 300.0,
-        "p": 1.0,  # normalisation order
-        "h_min": 1e-07,  # minimum allowed edge length
-        "h_max": 1.0,  # maximum allowed edge length
-    }}
-
-metric.set_parameters(mp)
-metric.interpolate(abs(gs) * ufl.Identity(dim))
-metric.normalise()
-
-
-adaptedMesh = adapt(mesh,  metric)
-
-VTKFile("result.pvd").write(adapted)
+VTKFile("results.pvd").write(adaptedMesh)
