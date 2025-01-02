dd11b17c6fda7bc1dab9b8526711ab43da90a8f0
StefanoFochesatto <54640332+StefanoFochesatto@users.noreply.github.com>
2025-01-01 15:28:23 -0900

    fixing metric example with new utility script

--- examples/metricAdaptation.py
+++ examples/metricAdaptation.py
@@ -3,13 +3,13 @@ import numpy as np
 from firedrake import *
 from animate import *   # see README.md regarding this dependency
 from firedrake.output import VTKFile
-from viamr.utility import ObstacleProblem
+from viamr.utility import SphereObstacleProblem
 from viamr import VIAMR
 
 
 initTriHeight = .1
 
-problem_instance = ObstacleProblem(TriHeight=initTriHeight)
+problem_instance = SphereObstacleProblem(TriHeight=initTriHeight)
 u, lb, mesh = problem_instance.solveProblem(mesh=None, u=None)
 viamr = VIAMR()
 adaptedMesh = viamr.metricrefine(mesh, u, lb)
