4ccd60cf4c3fa6d33d2515793a2959eaa7fbd99b
StefanoFochesatto <54640332+StefanoFochesatto@users.noreply.github.com>
2025-01-06 17:40:29 -0900

    comments

--- viamr/utility.py
+++ viamr/utility.py
@@ -13,28 +13,12 @@ class BaseObstacleProblem(ABC, OptionsManager):
         self.debug = kwargs.pop('debug', False)
 
     @abstractmethod
-    def setInitialMesh(self):
+    def getInitialMesh(self):
         pass
 
     @abstractmethod
-    def setBoundaryConditionsUFL(self, V, bdryID=None):
+    def getObstacle(self, V, bdry=None):
         pass
-
-    @abstractmethod
-    def setObstacleUFL(self, V):
-        pass
-
-    def getObstacle(self, V):
-        obsUFL = self.setObstacleUFL(V)
-
-        return Function(V).interpolate(obsUFL)
-
-    def getBoundaryConditions(self, V):
-        bdryUFL, bdryID = self.setBoundaryConditionsUFL(V)
-        bdry = Function(V).interpolate(bdryUFL)
-        bcs = DirichletBC(V, bdry, bdryID)
-
-        return bcs, bdry
 
     def solveProblem(self, mesh=None, u=None, bdry=None,  sp={
         "snes_vi_monitor": None,
@@ -51,7 +35,7 @@ class BaseObstacleProblem(ABC, OptionsManager):
         "pc_factor_mat_solver_type": "mumps"
     }):
         if mesh is None:
-            mesh = self.setInitialMesh()
+            mesh = self.getInitialMesh()
 
         V = FunctionSpace(mesh, "CG", 1)
 
@@ -60,8 +44,7 @@ class BaseObstacleProblem(ABC, OptionsManager):
         else:
             u = Function(V, name="u (FE soln)").interpolate(u)
 
-        lb = self.getObstacle(V)
-        bcs, _ = self.getBoundaryConditions(V)
+        lb, bcs, _ = self.getObstacle(V, bdry)
 
         v = TestFunction(V)
         F = inner(grad(u), grad(v)) * dx
@@ -77,15 +60,10 @@ class BaseObstacleProblem(ABC, OptionsManager):
 
 
 class SphereObstacleProblem(BaseObstacleProblem):
-    '''
-    Example for constructing an obstacle problem using abstract base classes to simplify usage.
-    All that is needed is to specify the initial mesh, obstacle UFL, and boundary conditions UFL expressions. 
-    '''
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def setInitialMesh(self):
+    def getInitialMesh(self):
         geo = SplineGeometry()
         geo.AddRectangle(p1=(-2, -2),
                          p2=(2, 2), bc="rectangle")
@@ -93,67 +71,45 @@ class SphereObstacleProblem(BaseObstacleProblem):
         mesh = Mesh(ngmsh)
         return mesh
 
-    def setObstacleUFL(self, V):
+    def getObstacleBC(self, V, bdry=None):
+        """
+        Sets up the obstacle problem by defining the obstacle function, sampled boundary condition function,
+        and the Dirichlet boundary conditions object.
+
+        Parameters:
+        V     : FunctionSpace where the problem is defined.
+        bdry  : Optional tuple specifying boundary indices. Defaults to all boundary indices (1, 2, 3, 4).
+
+        Returns:
+        psi   : Function representing the obstacle in the function space V.
+        gbdry : Function object corresponding to the sampled boundary condition.
+        bcs   : DirichletBC object that applies the boundary conditions to the problem.
+        """
+
         mesh = V.mesh()
         (x, y) = SpatialCoordinate(mesh)
         r = sqrt(x * x + y * y)
         r0 = 0.9
         psi0 = np.sqrt(1.0 - r0 * r0)
         dpsi0 = -r0 / psi0
-        obsUFL = conditional(le(r, r0), sqrt(
+        psi_ufl = conditional(le(r, r0), sqrt(
             1.0 - r * r), psi0 + dpsi0 * (r - r0))
-        return obsUFL
-
-    def setBoundaryConditionsUFL(self, V, bdryID=None):
-        mesh = V.mesh()
-        (x, y) = SpatialCoordinate(mesh)
-        r = sqrt(x * x + y * y)
+        psi = Function(V).interpolate(psi_ufl)
         afree = 0.697965148223374
         A = 0.680259411891719
         B = 0.471519893402112
+        gbdry_ufl = conditional(le(r, afree), psi_ufl, -A * ln(r) + B)
+        gbdry = Function(V).interpolate(gbdry_ufl)
 
-        obsUFL = self.setObstacleUFL(V)
-        bdryUFL = conditional(le(r, afree), obsUFL, -A * ln(r) + B)
+        if bdry is None:
+            bdry = (1, 2, 3, 4)
 
-        if bdryID is None:
-            bdryID = (1, 2, 3, 4)
-
-        return bdryUFL, bdryID
-
-
-class SpiralObstacleProblem(BaseObstacleProblem):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def setInitialMesh(self):
-        geo = SplineGeometry()
-        geo.AddRectangle(p1=(-1, -1),
-                         p2=(1, 1), bc="rectangle")
-        ngmsh = geo.GenerateMesh(maxh=self.TriHeight)
-        mesh = Mesh(ngmsh)
-        return mesh
-
-    def setObstacleUFL(self, V):
-        mesh = V.mesh()
-        (x, y) = SpatialCoordinate(mesh)
-        r = sqrt(x * x + y * y)
-        theta = atan2(y, x)
-        tmp = sin(2.0*pi/r + pi/2.0 - theta) + r * \
-            (r+1) / (r - 2.0) - 3.0 * r + 3.6
-        obsUFL = conditional(le(r, 1.0e-8), 3.6, tmp)
-        return obsUFL
-
-    def setBoundaryConditionsUFL(self, V, bdryID=None):
-        bdryUFL = Constant(0.0)
-        if bdryID is None:
-            bdryID = (1, 2, 3, 4)
-
-        return bdryUFL, bdryID
+        bcs = DirichletBC(V, gbdry, bdry)
+        return psi, gbdry, bcs
 
 
-# # Example usage
+# Example usage
 # if __name__ == '__main__':
-#     problem = SpiralObstacleProblem(TriHeight=.05)  # Instantiate your problem
-#     # Pass initial iterate, refined mesh, or solver parameter dictionary.
-#     solution, lower_bound, mesh = problem.solveProblem()
-#     print("Solution obtained successfully.")
+#    problem = SphereObstacleProblem()
+#    solution, lower_bound, mesh = problem.solveProblem()
+#    print("Solution obtained successfully.")
