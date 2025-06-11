8a99a081c39926d0c0add53a02762beb25ad4ba1
Ed Bueler <elbueler@alaska.edu>
2025-06-10 20:00:18 -0800

    working on it ...

--- examples/glacier/caps.sh
+++ examples/glacier/caps.sh
@@ -4,8 +4,8 @@
 MPG="mpiexec --bind-to hwthread --map-by core"
 P=12
 
-OPTS="-prob cap -elevdepend -m 20 -udo_n 1 -pcount 20"  # FIXME -udo_n 2 better?
-REFINE="-uniform 2 -refine 8"  # FIXME go to 2,9? 3,8?
+OPTS="-prob cap -elevdepend -m 20 -udo_n 2 -pcount 20"  # FIXME -udo_n 1 or -udo_n 2 better?
+REFINE="-uniform 3 -refine 9"  # FIXME ?
 
 ELA=1000
 BOX="-box 1000.0e3 1300.0e3 900.0e3 1200.0e3"
@@ -20,7 +20,7 @@ echo $CMD
 time $MPG -n $P $CMD
 
 ELA=600
-BOX="-box 1100.0e3 1400.0e3 1300.0e3 1600.0e3"
+BOX="-box 1100.0e3 1400.0e3 1400.0e3 1700.0e3"
 CMD="python3 steady.py $OPTS $REFINE $BOX -sELA $ELA -extractpvd result_sub_$ELA.pvd -opvd result_cap_$ELA.pvd"
 echo $CMD
 time $MPG -n $P $CMD
